b33c45037199e5d3e435ffd2440f5fee01ab4ee9
Winnie Carole Tongle Djoukeng <winnie.carole.tongle.djoukeng@mni.thm.de>
2023-05-28 09:06:17 +0200

    add logging level

--- restful-web-services/src/main/java/com/example/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloworldController.java
+++ restful-web-services/src/main/java/com/example/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloworldController.java
@@ -1,13 +1,17 @@
 package com.example.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloworldController {
-    @RequestMapping(path = "/hello-world")
+    @GetMapping(path = "/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloworldBean(String message) {
+        return new HelloWorldBean("hello world");
     }
 }
